6fa5b790836af3576b85f59bb6b60e01b9420b6a
FranOrtega01 <franortega.wg@gmail.com>
2024-05-16 17:25:09 -0300

    Home

--- src/Components/Test/Test.jsx
+++ src/Components/Test/Test.jsx
@@ -6,15 +6,16 @@ import { results } from '../../assets/data';
 import fb from '../../assets/fb.png'
 import ig from '../../assets/ig.png'
 import wpp from '../../assets/wpp.png'
-
+import logo from '../../assets/onlyLogo.png'
 
 export const Test = () => {
 
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(-1)
   const [question, setQuestion] = useState(data[index])
   const [score, setScore] = useState(0)
   const [selectedValue, setSelectedValue] = useState(0)
   const [loading, setLoading] = useState(false)
+
   const checkAns = (ans) => {
     const optionsElements = document.querySelectorAll('div ul li');
     optionsElements.forEach((option, index) => {
@@ -45,14 +46,12 @@ export const Test = () => {
     if (score > 17) i = 3;
     const paragraphs = results[i]?.p.map((txt, idx) => {
       if (idx === results[i]?.p.length - 1) {
-        // Si es el último párrafo, agrega el span al final
         return (
           <p key={idx}>
-            {txt} <span>{results[i]?.span}</span>
+            {txt} <span>{results[i]?.span}</span>.
           </p>
         );
       } else {
-        // Para otros párrafos, solo renderiza el texto del párrafo
         return <p key={idx}>{txt}</p>;
       }
     });
@@ -60,8 +59,8 @@ export const Test = () => {
     return <>{paragraphs}</>;
   }
 
-  const resetTest = () => {
-    setIndex(0);
+  const resetTest = (idx) => {
+    setIndex(idx);
     setScore(0);
     setSelectedValue(0);
   }
@@ -81,48 +80,63 @@ export const Test = () => {
 
   return (
     <div className='container'>
-      <h1>UNDelta Test</h1>
-      {index === data.length ?
-        loading ? <Spinner /> :
-        <div className='results'>
-          {getResults()}
-          <div>
-            <button>Volver al Menú</button>
-            <button onClick={resetTest}>Rehacer Test</button>
+      <div className='header'>
+        <img src={logo} style={{height:50}} alt="UNDelta"/>
+        <h1>UNDelta Test</h1>
+      </div>
+      {
+        index === -1 ?
+
+          // Home
+          <div className="home">
+            <p>¿Tenés ganas de estudiar y todavía no sabés <span>dónde</span> ni <span>qué</span>? La parte de "dónde" está resuelta, ahora que llegó UNDelta a la región. Para la parte de qué, el <span>"UNDelta Test"</span> es para vos.</p>
+            <p>
+           Tomate 5 minutos, respondé unas sencillas preguntas y descubrí cuál es la mejor opción para tu futuro. ¿Estás listo?</p>
+            <button onClick={handleQuestion}>Comenzar Test</button>
           </div>
-          <div className='social'>
-            <a href="https://www.facebook.com/UNIVERSIDADDELTA" target="_blank" rel="noopener noreferrer">
-              <img src={fb} alt="Facebook" />
-            </a>
-            <a href="https://www.instagram.com/universidaddelta?igsh=ajg3Y3NjYjlvNTdp" target="_blank" rel="noopener noreferrer">
-              <img src={ig} alt="Instagram" />
-            </a>
-            <a href="https://wa.link/2bnt9s" target="_blank" rel="noopener noreferrer">
-              <img src={wpp} alt="WhatsApp" />
-            </a>
-          </div>
-        </div>
-        :
-        <>
-          
-          <h2>{index + 1}. {question?.question}</h2>
-          <ul>
-            {question?.options.map((q, i) => <li className='shadow' onClick={(e) => checkAns(i)}>{q}</li>)}
-          </ul>
-          <button disabled={!selectedValue} onClick={handleQuestion}>Siguiente</button>
-          <div className="index">Pregunta {index + 1} de {data.length}</div>
-        </>
+
+          :
+          index === data.length ?
+            loading ? <Spinner text={"Analizando respuestas..."} /> :
+              <div className='results'>
+                {getResults()}
+                <div>
+                  <button onClick={() => resetTest(-1)}>Volver al Menú</button>
+                  <button onClick={() => resetTest(0)}>Rehacer Test</button>
+                </div>
+                <div className='social'>
+                  <a href="https://www.facebook.com/UNIVERSIDADDELTA" target="_blank" rel="noopener noreferrer">
+                    <img src={fb} alt="Facebook" />
+                  </a>
+                  <a href="https://www.instagram.com/universidaddelta?igsh=ajg3Y3NjYjlvNTdp" target="_blank" rel="noopener noreferrer">
+                    <img src={ig} alt="Instagram" />
+                  </a>
+                  <a href="https://wa.link/2bnt9s" target="_blank" rel="noopener noreferrer">
+                    <img src={wpp} alt="WhatsApp" />
+                  </a>
+                </div>
+              </div>
+            :
+            <>
+              <h2>{index + 1}. {question?.question}</h2>
+              <ul>
+                {question?.options.map((q, i) => <li className='shadow' onClick={(e) => checkAns(i)}>{q}</li>)}
+              </ul>
+              <button disabled={!selectedValue} onClick={handleQuestion}>Siguiente</button>
+              <div className="index">Pregunta {index + 1} de {data.length}</div>
+            </>
       }
     </div>
   )
 }
 
-const Spinner = () => {
+const Spinner = (props) => {
 
   return (
     <div className="spinner-container">
       <div className="spinner"></div>
-      <p>Analizando respuestas...</p>
+
+      {props.text && <p>{props.text}</p>}
     </div>
   )
 }
